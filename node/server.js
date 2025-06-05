e232beb2af72728484e31cccb61c96974158621c
lzam <110193626+lzam0@users.noreply.github.com>
2025-06-05 16:29:39 +0100

    Routing to index page working
    Index page routing working

--- node/server.js
+++ node/server.js
@@ -8,7 +8,21 @@ const session = require('express-session');
 // Load environment variables from the .env file
 require('dotenv').config();
 
+// Run off .env file for port
 const port = process.env.PORT
+
+// Set views and view engine
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+// Middleware to parse form data
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
 // Start the server
 const server = app.listen(port, () => {
