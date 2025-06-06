d958b05c02c3b0cbca4b7474fe623c47c9239684
lzam <110193626+lzam0@users.noreply.github.com>
2025-06-06 13:58:56 +0100

    test folder added

--- node/routes/authRouter.js
+++ node/routes/authRouter.js
@@ -2,20 +2,21 @@ const express = require('express');
 const router = express.Router();
 const AuthController = require('../controllers/authController');
 
-// Login page (GET)
+// Login page GET request
 router.get('/login', (req, res) => {
   res.render('login');
 });
 
-// Login (POST)router.post('/login', (req, res, next) => {
+// Login POST request
 router.post('/login', (req, res, next) => {
   console.log('POST /login received with body:', req.body);
   next();  // important!
 }, AuthController.login);
 
+// Logout POST request
 router.post('/logout', (req, res) => {
   res.clearCookie('token');  // Clear the cookie holding the JWT
-  res.redirect('/login');    // Or send JSON response if API
+  res.redirect('/login');
 });
 
 module.exports = router;
