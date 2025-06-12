a0ce0071ee5e012821f5ce7631fcc2dfe2206fa2
louisoggy <101870549+louisoggy@users.noreply.github.com>
2025-06-12 13:02:01 +0100

    small update

---
+++ node/models/articles.js
@@ -0,0 +1,4 @@
+module.exports = [
+  { id: 0, title: "How to Act During Floods", author: "Makers Valley Flood Saftey" },
+  { id: 1, title: "Article", author: "Not made yet" }
+];
--- node/routes/infoRouter.js
+++ node/routes/infoRouter.js
@@ -1,22 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
-const articles = [
-  { id: 0, title: "How to Act During Floods", author: "Makers Valley Flood Saftey" },
-  { id: 1, title: "Article", author: "Not made yet" }
-];
+const articles = require('../models/articles');
 
 router.get('/information', (req, res) => {
   res.render('information', { user: req.user, articles });
 });
 
 router.get('/information/article/0', (req, res) => {
-  res.render('info0', { user: req.user });
+  res.render('info0', { user: req.user, articles });
 });
 
 router.get('/information/article/:id', (req, res) => {
   const articleId = parseInt(req.params.id, 10);
-  // Check if exists
   const article = articles.find(a => a.id === articleId);
   if (!article || articleId === 0) {
     return res.send(`
